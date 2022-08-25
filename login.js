52680769b10b0848ff455d79a695aced813aad53
Amit Bhutani <107975368+amss-22@users.noreply.github.com>
2022-08-25 23:52:00 +0530

    added some files

--- login.js
+++ login.js
@@ -1,3 +1,23 @@
 let forget=()=>{
     window.location.href="forget.html"
 }
+
+let but=document.getElementById("nx")
+let head=document.getElementById("hd")
+let div1=document.getElementById("emy")
+let div2=document.getElementById("pussy")
+
+div1.classList.remove("passh")
+let button=document.getElementById("next").addEventListener("click",function(event){
+
+  event.preventDefault()
+  head.innerText="Password"
+  div1.classList.add("passh")
+  div2.classList.remove("passh")
+  but.innerText="Login"
+  but.addEventListener("click",function(){
+
+    //here full login insertion 
+    window.location.href="homepage.html"
+  })
+})
