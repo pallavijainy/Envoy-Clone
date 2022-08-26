009a72cd0ace0956dd065212aaa0ae31f34f9424
Nitin Sharma <95009807+ignitinsharma@users.noreply.github.com>
2022-08-27 00:59:06 +0530

    nav

--- component/footer.js
+++ component/footer.js
@@ -1,19 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>footer</title>
-    <link rel="stylesheet" href="./navbar.css">
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
-    />
-    <link rel="stylesheet" href="./footer.css" />
-  </head>
-  <body>
-    <div id="navbarid"></div>
+function footer() {
+    return `
     <div id="maindiv">
       <div id="footerbar">
         <div id="footer_div">
@@ -112,14 +98,7 @@
           <a href=""><i class="fa fa-linkedin" style="font-size: 20px"></i></a>
         </div>
       </div>
-    </div>
-  </body>
-</html>
-
-<script type="module">
-  import navbar from './component/navbar.js'
-  console.log(navbar);
-  let navbox=document.getElementById('navbarid');
-  navbox.innerHTML=navbar();
-
-</script>
+    </div>`;
+  };
+  
+  export default footer;
