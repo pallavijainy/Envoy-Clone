88497c1b265b1aec3246492c267894c4f18681d2
Nitin Sharma <95009807+ignitinsharma@users.noreply.github.com>
2022-08-28 13:49:55 +0530

    git add new files

---
+++ component/footer.js
@@ -0,0 +1,105 @@
+function footer() {
+    return `
+    <div id="navbarid"></div>
+    <div id="maindiv">
+      <div id="footerbar">
+        <div id="footer_div">
+          <h1 style="color: #fa4338; font-size: 40px">Try Envoy for free</h1>
+          <p style="color: #3f4450; font-weight: 600">
+            Transforming your workplace is easier than you think. Get started in
+            minutes.
+          </p>
+        </div>
+        <div id="form_div">
+          <input type="text" placeholder="Enter your mail" id="forminput" />
+          <button id="getstarted">Get Started</button>
+        </div>
+      </div>
+
+      <div id="mobiledivmain">
+        <p id="mobilediv">
+          <i class="fa fa-phone"></i> Reach out to our sales team today at
+          <span id="red">(877) 652-2808</span> or schedule a meeting with one of
+          our experts <span id="red">here.</span>
+        </p>
+      </div>
+
+      <div id="mainfooter">
+        <div id="Productsdiv">
+          <ul>
+            <h4>Products</h4>
+            <li><a href="">Protect</a></li>
+            <li><a href="">Desks</a></li>
+            <li><a href="">Visitors</a></li>
+            <li><a href="">Rooms</a></li>
+            <li><a href="">Deliveries</a></li>
+            <li><a href="">Mobile</a></li>
+            <li><a href="">Integrations</a></li>
+            <li><a href="">Partnerships</a></li>
+            <li><a href="">Pricing</a></li>
+            <li><a href="">Get a quote</a></li>
+          </ul>
+        </div>
+        <div id="Productsdiv">
+          <ul>
+            <h4>Solutions</h4>
+            <li><a href="">Hybrid work</a></li>
+            <li><a href="">Enterprise</a></li>
+            <li><a href="">Security</a></li>
+            <li><a href="">Space management</a></li>
+            <li><a href="">Health and safety</a></li>
+            <li><a href="">Workplace experience</a></li>
+            <li><a href="">Compliance</a></li>
+            <li><a href="">Touchless sign-in</a></li>
+            <li><a href="">Access control</a></li>
+            <li><a href="">Efficiency</a></li>
+            <li><a href="">Experience</a></li>
+          </ul>
+        </div>
+        <div id="Productsdiv">
+          <ul>
+            <h4>Resources</h4>
+            <li><a href="">Hybrid work resource hub</a></li>
+            <li><a href="">Help center</a></li>
+            <li><a href="">Return to Workplace Index</a></li>
+            <li><a href="">Envoy Store</a></li>
+            <li><a href="">Customer stories</a></li>
+            <li><a href="">Demo</a></li>
+            <li><a href="">Compliance tips</a></li>
+            <li><a href="">Developers</a></li>
+            <li><a href="">Glossary</a></li>
+          </ul>
+        </div>
+        <div id="Productsdiv">
+          <ul>
+            <h4>Company</h4>
+            <li><a href="">About Envoy</a></li>
+            <li><a href="">Vision</a></li>
+            <li><a href="">Jobs</a></li>
+            <li><a href="">Blog</a></li>
+            <li><a href="">Data security</a></li>
+            <li><a href="">Press</a></li>
+            <li><a href="">Partner</a></li>
+          </ul>
+        </div>
+      </div>
+
+      <div id="social_media">
+        <div>
+          <a href="">Contact</a>
+          <a href="">Status</a>
+          <a href="">Terms of Service</a>
+          <a href="">Privacy Policy</a>
+        </div>
+        <div id="social_media_div2">
+          <a href=""
+            ><i class="fa fa-facebook-f" style="font-size: 20px"></i
+          ></a>
+          <a href=""><i class="fa fa-twitter" style="font-size: 20px"></i></a>
+          <a href=""><i class="fa fa-linkedin" style="font-size: 20px"></i></a>
+        </div>
+      </div>
+    </div>`;
+  };
+  
+  export default footer;
